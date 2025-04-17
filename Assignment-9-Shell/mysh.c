1435b70bd9f65a756bf60754a14943911ee6a5d0
Uttam Mahata <uttam-mahata-cs@outlook.com>
2025-04-17 12:30:23 +0530

    refactor includes for better readability

--- Assignment-9-Shell/mysh.c
+++ Assignment-9-Shell/mysh.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>     // For fork, execvp, chdir, getcwd, pid_t
-#include <sys/wait.h>   // For waitpid
-#include <sys/types.h>  // For pid_t
-#include <errno.h>      // For perror
+#include <unistd.h>     
+#include <sys/wait.h>   
+#include <sys/types.h>  
+#include <errno.h>
 
 #define MAX_LINE_LEN 1024
 #define MAX_ARGS 64
